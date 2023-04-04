b0cad4a4714a9c512744172c2037b01f65ea5fd7
chattie <christuaik@gmail.com>
2023-04-04 10:02:42 -0700

    complete, passing

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-function submitData(name, email){
+function submitData(n, e){
     const newObj = {
-        userName: name,
-        userEmail: email,
+        name: n,
+        email: e,
     }
     return fetch('http://localhost:3000/users', {
         method: 'POST',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type':'application/json',
+            'Accept': 'application/json'
         },
-        body:JSON.stringify(newObj),
+        body: JSON.stringify(newObj),
     })
     .then(res => res.json())
     .then(data => render(data))
     .catch(function(error){
         let errorElement = document.createElement('p');
-        errorElement = 'ERROOOOOOR';
+        errorElement = 'Unauthorized Access';
         document.body.append(errorElement);
     })
 }
 
 function render(obj){
-    let eID = document.createElement('p');
-    let eName = document.createElement('p');
-    let eEmail = document.createElement('p');
-    eID.innerText = 'ID: ' + obj.id;
-    eName.innerText = 'name: ' + obj.userName;
-    eEmail.innerText = 'email: ' + obj.userEmail;
-    document.body.append(eID);
-    eID.append(eName);
-    eID.append(eEmail);
+//    let eID = document.createElement('p');
+//    let eName = document.createElement('p');
+//    let eEmail = document.createElement('p');
+//    eID.innerText = obj.id;
+//    eName.innerText = 'name: ' + obj.userName;
+//    eEmail.innerText = 'email: ' + obj.userEmail;
+    document.body.append(obj.id);
+//    eID.append(eName);
+//    eID.append(eEmail);
 }
